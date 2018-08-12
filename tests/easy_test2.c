6a103d251c29cc7881d2aba19b0a696520362ce8
nsimonov <nicksimonovv@gmail.com>
2018-08-12 16:53:01 +0300

    [nm - otool] add tests

---
+++ tests/easy_test2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int global = 40; 
+
+int main(void)
+{
+	printf("GLOBAL VAR: %d\n", global);
+	return (0);
+}
---
+++ tests/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char *ptr = "test";
+char arr[2];
+
+int main(void)
+{
+	arr[0] = 'a';
+	printf("hello World\n");
+	return 0;
+}
